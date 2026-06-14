91142053f9c416666f8c6211da7dfc0775a35768
dam.wan <dam.wan@igsl-group.com>
2026-06-14 23:48:46 +0800

    Add files via upload

--- SysRep.py
+++ SysRep.py
@@ -13,7 +13,7 @@ import sys
 import logging
 import json
 
-POSTGRES_CONN = {
+conn_str = {
    "host":"192.168.0.186",
    "dbname":"AdventureWork",
    "user":"intellipaas",
@@ -34,29 +34,38 @@ POSTGRES_CONN = {
 # columns: ((<field>,<header>,<alignment>,<width>),(<field...)...)
 #
 #***************************************************************************************************************************
-jsn = []
 with open("spec.json","r") as f:
-     jsn = json.load(f)
+    jsn = json.load(f)
 
-print (jsn)
-print (jsn["margin"])
-print (jsn["cols"][0]["lbl"])
+conn = psycopg2.connect(**conn_str)
+#tbl = conn.cursor()
+tbl = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+tbl.execute(jsn["sql"])
+if jsn.get("columns"):
+   spec = {col["field"]: col["header"] for col in jsn["columns"]}
+   spec1 = {col["field"]: col["header"] for col in jsn["columns"]}
+else:
+   varCols = [desc[0] for desc in tbl.description]
+   spec = {col: col for col in varCols}
+print (spec, spec.get("header"))
 
-conn = psycopg2.connect(**POSTGRES_CONN)
-tbl  = conn.cursor()
+f = open(f"{jsn.get("rpt")}.{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv","a", newline="", encoding="utf-8")
+rpt = csv.writer(f)
+rpt.writerow(list(spec.values()))
 
-tbl.execute("SELECT * FROM tmp_stg.sys_dict")
-cols = [desc[0] for desc in tbl.description]
+sys.exit()
+
 rs = tbl.fetchall()
 
 for r in rs:
-    row_dict = dict(zip(cols, r))
-#    print(row_dict["code"])
-    print (jsn["rpt"]," ", end="")
-    print (jsn["fmt"]," ", end="")
-    for c in jsn["cols"]:
-        print (c["lbl"],": ", row_dict[c["lbl"]]," ", end="")
-    print()
+    rec = [r[c] for c in spec.keys()]
+    for c in spec.keys():
+        rec.append(r[c])
+#    for a in rec: print (a,end=",")
+#    for a in spec: print (jsn.get("columns").get(a).get("width"),end=",")
+#    print (rec)
+    rpt.writerow(rec)
+
 
 tbl.close()
 conn.close()
